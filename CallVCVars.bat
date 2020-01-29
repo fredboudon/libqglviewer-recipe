30cde317467cf945d7738e8e9f164b23b698241b
Frederic Boudon <frederic.boudon@cirad.fr>
2020-01-29 15:10:28 +0100

    test on appveyor.yml

--- CallVCVars.bat
+++ CallVCVars.bat
@@ -1,7 +1,7 @@
-IF %ARCH% == 64 (
-  echo "VS2017 64bit"
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-) ELSE (
+if "%PLATFORM%" == "x86" (
   echo "VS2017 32bit"
   call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+) ELSE (
+  echo "VS2017 64bit"
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 )
