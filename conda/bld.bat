9abb581bddf4e9a336a9af51f3ba3da993320f9a
Frederic Boudon <frederic.boudon@cirad.fr>
2020-02-03 12:57:27 +0100

    mv activation in appveyor.yml

--- conda/bld.bat
+++ conda/bld.bat
@@ -2,11 +2,6 @@
 mkdir build
 cd build
 
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-if errorlevel 1 exit 1
-
-
 qmake ../libQGLViewer.pro PREFIX=%LIBRARY_PREFIX% NO_QT_VERSION_SUFFIX=1 
 if errorlevel 1 exit 1
 
