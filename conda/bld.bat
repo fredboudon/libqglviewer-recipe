6c0893404029c59c9196f9e915224251363e13bb
Frederic Boudon <frederic.boudon@cirad.fr>
2020-02-03 12:32:50 +0100

    add error test

--- conda/bld.bat
+++ conda/bld.bat
@@ -3,11 +3,17 @@ mkdir build
 cd build
 
 %VSINSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat
+if errorlevel 1 exit 1
+
 
 qmake ../libQGLViewer.pro PREFIX=%LIBRARY_PREFIX% NO_QT_VERSION_SUFFIX=1 
+if errorlevel 1 exit 1
 
 nmake release
+if errorlevel 1 exit 1
 nmake install
+if errorlevel 1 exit 1
+
 cd ..
 
 COPY QGLViewer\QGLViewer2.dll %LIBRARY_BIN%
@@ -16,3 +22,4 @@ mkdir  %LIBRARY_INC%\QGLViewer
 COPY QGLViewer\*.h %LIBRARY_INC%\QGLViewer
 mkdir  %LIBRARY_INC%\QGLViewer\VRender
 COPY QGLViewer\VRender\*.h %LIBRARY_INC%\QGLViewer\VRender
+if errorlevel 1 exit 1
